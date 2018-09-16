eb982cdc5f613ae1b85351861511273bbf1c6053
BogerChan <bogerchan@hotmail.com>
2018-09-16 21:15:25 +0800

    [UPDATE] support audio record

--- core/src/main/java/me/bogerchan/niervisualizer/NierVisualizerManager.kt
+++ core/src/main/java/me/bogerchan/niervisualizer/NierVisualizerManager.kt
@@ -3,6 +3,7 @@ package me.bogerchan.niervisualizer
 import android.media.audiofx.Visualizer
 import android.view.SurfaceView
 import me.bogerchan.niervisualizer.core.NierVisualizerRenderWorker
+import me.bogerchan.niervisualizer.core.PeriodWorker
 import me.bogerchan.niervisualizer.renderer.IRenderer
 import java.lang.ref.WeakReference
 
@@ -13,37 +14,24 @@ import java.lang.ref.WeakReference
  */
 class NierVisualizerManager {
 
+    companion object {
+        private const val DATA_SOURCE_TYPE_UNKNOWN = -1
+        private const val DATA_SOURCE_TYPE_VISUALIZER = 0
+        private const val DATA_SOURCE_TYPE_OUTSIDE = 1
+    }
+
     private var mVisualizer: Visualizer? = null
     private val mRenderer by lazy { NierVisualizerRenderWorker() }
     private var mWaveBuffer: ByteArray? = null
     private var mFftBuffer: ByteArray? = null
     private val mStateBlock = Object()
 
-    private val mDataCaptureListener by lazy {
-        object : Visualizer.OnDataCaptureListener {
-            override fun onFftDataCapture(visualizer: Visualizer?, fft: ByteArray?, samplingRate: Int) {
-                val fftBuffer = mFftBuffer ?: return
-                if (fft == null || fft.size != fftBuffer.size) {
-                    return
-                }
-                System.arraycopy(fft, 0, fftBuffer, 0, fft.size)
-                mRenderer.updateFftData(fftBuffer)
-            }
+    private var renderViewWR: WeakReference<SurfaceView>? = null
+    private var renderers: Array<IRenderer>? = null
+    private var mPeriodWorker: PeriodWorker? = null
+    private var mDataSourceType: Int = DATA_SOURCE_TYPE_UNKNOWN
+    private var mDataCaptureSize: Int = 0
 
-            override fun onWaveFormDataCapture(visualizer: Visualizer?, waveform: ByteArray?, samplingRate: Int) {
-                val waveBuffer = mWaveBuffer ?: return
-                if (waveform == null || waveform.size != waveBuffer.size) {
-                    return
-                }
-                System.arraycopy(waveform, 0, waveBuffer, 0, waveform.size)
-                mRenderer.updateWaveData(waveBuffer)
-            }
-
-        }
-    }
-
-    var renderViewWR: WeakReference<SurfaceView>? = null
-    var renderers: Array<IRenderer>? = null
 
     /**
      * Initialize Nier visualizer, you should use it in [android.app.Activity.onCreate].
@@ -51,17 +39,68 @@ class NierVisualizerManager {
      */
     fun init(audioSession: Int) {
         synchronized(mStateBlock) {
-            val visualizer = Visualizer(audioSession).apply {
+            mVisualizer = Visualizer(audioSession).apply {
                 enabled = false
                 captureSize = 512
                 scalingMode = Visualizer.SCALING_MODE_NORMALIZED
                 measurementMode = Visualizer.MEASUREMENT_MODE_NONE
-                setDataCaptureListener(mDataCaptureListener, Visualizer.getMaxCaptureRate(), true, true)
-                enabled = true
+                setDataCaptureListener(object : Visualizer.OnDataCaptureListener {
+                    override fun onFftDataCapture(visualizer: Visualizer?, fft: ByteArray?, samplingRate: Int) {
+                        val fftBuffer = mFftBuffer ?: return
+                        if (fft == null || fft.size != fftBuffer.size) {
+                            return
+                        }
+                        System.arraycopy(fft, 0, fftBuffer, 0, fft.size)
+                        mRenderer.updateFftData(fftBuffer)
+                    }
+
+                    override fun onWaveFormDataCapture(visualizer: Visualizer?, waveform: ByteArray?, samplingRate: Int) {
+                        val waveBuffer = mWaveBuffer ?: return
+                        if (waveform == null || waveform.size != waveBuffer.size) {
+                            return
+                        }
+                        System.arraycopy(waveform, 0, waveBuffer, 0, waveform.size)
+                        mRenderer.updateWaveData(waveBuffer)
+                    }
+
+                }, Visualizer.getMaxCaptureRate(), true, true)
+            }.apply {
+                mDataCaptureSize = captureSize.apply {
+                    mWaveBuffer = ByteArray(this)
+                    mFftBuffer = ByteArray(this)
+                }
             }
-            mWaveBuffer = ByteArray(visualizer.captureSize)
-            mFftBuffer = ByteArray(visualizer.captureSize)
-            mVisualizer = visualizer
+            mDataSourceType = DATA_SOURCE_TYPE_VISUALIZER
+        }
+    }
+
+    fun init(dataSource: NVDataSource) {
+        synchronized(mStateBlock) {
+            mDataCaptureSize = dataSource.getDataLength().apply {
+                mWaveBuffer = ByteArray(this)
+                mFftBuffer = ByteArray(this)
+            }
+            mPeriodWorker = PeriodWorker(dataSource.getDataSamplingInterval()) {
+                dataSource.apply {
+                    fetchFftData()?.apply {
+                        val fftBuffer = mFftBuffer ?: return@apply
+                        if (size != fftBuffer.size) {
+                            throw IllegalStateException("NVDataSource must provide correct fft data size as it's getDataLength() says.")
+                        }
+                        System.arraycopy(this, 0, fftBuffer, 0, size)
+                        mRenderer.updateFftData(fftBuffer)
+                    }
+                    fetchWaveData()?.apply {
+                        val waveBuffer = mWaveBuffer ?: return@apply
+                        if (size != waveBuffer.size) {
+                            throw IllegalStateException("NVDataSource must provide correct wave data size as it's getDataLength() says.")
+                        }
+                        System.arraycopy(this, 0, waveBuffer, 0, size)
+                        mRenderer.updateFftData(waveBuffer)
+                    }
+                }
+            }
+            mDataSourceType = DATA_SOURCE_TYPE_OUTSIDE
         }
     }
 
@@ -74,12 +113,20 @@ class NierVisualizerManager {
             renderers = null
             mRenderer.stop()
             mRenderer.quit()
-            mVisualizer?.enabled = false
-            mVisualizer?.setDataCaptureListener(null, Visualizer.getMaxCaptureRate(), true, true)
-            mVisualizer?.release()
-            mVisualizer = null
             mWaveBuffer = null
             mFftBuffer = null
+            when (mDataSourceType) {
+                DATA_SOURCE_TYPE_VISUALIZER -> {
+                    mVisualizer?.enabled = false
+                    mVisualizer?.setDataCaptureListener(null, Visualizer.getMaxCaptureRate(), true, true)
+                    mVisualizer?.release()
+                    mVisualizer = null
+                }
+                DATA_SOURCE_TYPE_OUTSIDE -> {
+                    mPeriodWorker?.stop()
+                    mPeriodWorker = null
+                }
+            }
         }
     }
 
@@ -90,14 +137,27 @@ class NierVisualizerManager {
      */
     fun start(view: SurfaceView, newRenderers: Array<IRenderer>) {
         synchronized(mStateBlock) {
-            val visualizer = mVisualizer
-                    ?: throw IllegalStateException("You must call NierVisualizerManager.init() first!")
             if (newRenderers.isEmpty()) {
                 throw IllegalStateException("Renders is empty!")
             }
+            when (mDataSourceType) {
+                DATA_SOURCE_TYPE_VISUALIZER -> {
+                    val visualizer = mVisualizer
+                            ?: throw IllegalStateException("You must call NierVisualizerManager.init() first!")
+                    visualizer.enabled = true
+                }
+                DATA_SOURCE_TYPE_OUTSIDE -> {
+                    val periodWorker = mPeriodWorker
+                            ?: throw IllegalStateException("You must call NierVisualizerManager.init() first!")
+                    periodWorker.start()
+                }
+                else -> {
+                    throw IllegalStateException("You must call NierVisualizerManager.init() first!")
+                }
+            }
             renderViewWR = WeakReference(view)
             renderers = newRenderers
-            mRenderer.start(NierVisualizerRenderWorker.RenderCore(visualizer.captureSize, view, newRenderers))
+            mRenderer.start(NierVisualizerRenderWorker.RenderCore(mDataCaptureSize, view, newRenderers))
         }
     }
 
@@ -109,6 +169,16 @@ class NierVisualizerManager {
             renderViewWR = null
             renderers = null
             mRenderer.stop()
+            when (mDataSourceType) {
+                DATA_SOURCE_TYPE_VISUALIZER -> {
+                    mVisualizer?.enabled = false
+                }
+                DATA_SOURCE_TYPE_OUTSIDE -> {
+                    mPeriodWorker?.stop()
+                }
+                else -> {
+                }
+            }
         }
     }
 
@@ -118,6 +188,16 @@ class NierVisualizerManager {
     fun pause() {
         synchronized(mStateBlock) {
             mRenderer.pause()
+            when (mDataSourceType) {
+                DATA_SOURCE_TYPE_VISUALIZER -> {
+                    mVisualizer?.enabled = false
+                }
+                DATA_SOURCE_TYPE_OUTSIDE -> {
+                    mPeriodWorker?.pause()
+                }
+                else -> {
+                }
+            }
         }
     }
 
@@ -127,6 +207,46 @@ class NierVisualizerManager {
     fun resume() {
         synchronized(mStateBlock) {
             mRenderer.resume()
+            when (mDataSourceType) {
+                DATA_SOURCE_TYPE_VISUALIZER -> {
+                    mVisualizer?.enabled = true
+                }
+                DATA_SOURCE_TYPE_OUTSIDE -> {
+                    mPeriodWorker?.resume()
+                }
+                else -> {
+                }
+            }
         }
+    }
+
+    /**
+     * An interface used for providing fft data or wave data, these data will be transformed to visual effect.
+     */
+    interface NVDataSource {
+
+        /**
+         * Tell the manager about the data sampling interval.
+         * @return the data sampling interval which is millisecond of unit.
+         */
+        fun getDataSamplingInterval(): Long
+
+        /**
+         * Tell the manager about the data length of fft data or wave data.
+         * @return the data length of fft data or wave data.
+         */
+        fun getDataLength(): Int
+
+        /**
+         * The manager will fetch fft data by it.
+         * @return the fft data, null will be ignored by the manager.
+         */
+        fun fetchFftData(): ByteArray?
+
+        /**
+         * The manager will fetch wave data by it.
+         * @return the wave data, null will be ignored by the manager.
+         */
+        fun fetchWaveData(): ByteArray?
     }
 }
